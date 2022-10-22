d8ba23c051d6cc25522cd1f6b549d63533771e2a
Ling-yunchi <wr020210@gmail.com>
2022-10-22 10:26:14 +0800

    feat: finish lexical analysis

--- src/parser/lexical_analysis.rs
+++ src/parser/lexical_analysis.rs
@@ -1,11 +1,12 @@
 use core::num;
-use std::error::Error;
+use std::{error::Error, process::id};
 
 use super::error::LexicalError;
 
 /// 词法分析
-pub fn lexical_analysis(input: String) {
-    let mut after_preprocessing = preprocess(input);
+pub fn lexical_analysis(input: String) -> Result<(Vec<Token>, bool), LexicalError> {
+    let after_preprocessing = preprocess(input)?;
+    Ok(process(after_preprocessing))
 }
 
 #[rustfmt::skip]
@@ -32,30 +33,52 @@ const OPERATOR: [&str; 24] = [
 #[rustfmt::skip]
 const DELIMITERS: [&str; 9] = ["{", "}", "[", "]", "(", ")", ",", ".", ";"];
 
+#[derive(Debug)]
 pub struct Token {
     pub token_type: TokenType,
     pub token_value: String,
 }
 
+impl PartialEq for Token {
+    fn eq(&self, other: &Self) -> bool {
+        self.token_type == other.token_type && self.token_value == other.token_value
+    }
+}
+
+#[derive(Debug)]
 pub enum TokenType {
     Keyword,
     Identifier,
     Constant,
     Operator,
     Delimiter,
+    Error(LexicalError),
+}
+
+impl PartialEq for TokenType {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Error(l0), Self::Error(r0)) => l0.to_string() == r0.to_string(),
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
 }
 
 /// 处理
-fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
+fn process(input: Vec<String>) -> (Vec<Token>, bool) {
     let mut result = Vec::new();
+    let mut error = false;
+
     for line in input {
         let mut chars = line.chars().peekable();
 
         while let Some(char) = chars.next() {
+            // 空格 跳过
             if char == ' ' {
                 continue;
             }
 
+            // 分隔符
             if DELIMITERS.contains(&char.to_string().as_str()) {
                 result.push(Token {
                     token_type: TokenType::Delimiter,
@@ -64,6 +87,7 @@ fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
                 continue;
             }
 
+            // 运算符
             if OPERATOR.contains(&char.to_string().as_str()) {
                 let mut operator = char.to_string();
 
@@ -82,11 +106,12 @@ fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
                 continue;
             }
 
-            if char.is_numeric() {
+            // 数字
+            if char.is_ascii_digit() {
                 let mut number = char.to_string();
 
                 while let Some(next_char) = chars.peek() {
-                    if next_char.is_numeric() || *next_char == '.' {
+                    if next_char.is_ascii_digit() || *next_char == '.' {
                         number.push(*next_char);
                         chars.next();
                     } else {
@@ -96,7 +121,6 @@ fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
 
                 // 防止出现数字开头的非法标识符
                 if number.len() == line.len()
-                    || chars.peek().unwrap().is_numeric()   // 123abc
                     || OPERATOR.contains(&chars.peek().unwrap().to_string().as_str())
                     || DELIMITERS.contains(&chars.peek().unwrap().to_string().as_str())
                     || chars.peek().unwrap() == &' '
@@ -104,10 +128,13 @@ fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
                     if number.contains('.') {
                         let idx = number.find('.').unwrap();
                         if number[idx + 1..].contains('.') {
-                            return Err(Box::new(LexicalError::new(&format!(
-                                "Invalid float number {}",
-                                number,
-                            ))));
+                            result.push(Token {
+                                token_type: TokenType::Error(LexicalError::new(
+                                    "Invalid float number",
+                                )),
+                                token_value: number,
+                            });
+                            error = true;
                         } else {
                             result.push(Token {
                                 token_type: TokenType::Constant,
@@ -115,20 +142,135 @@ fn process(input: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
                             });
                         }
                     }
+                } else {
+                    while let Some(next_char) = chars.peek() {
+                        if next_char == &' '
+                            || OPERATOR.contains(&next_char.to_string().as_str())
+                            || DELIMITERS.contains(&next_char.to_string().as_str())
+                        {
+                            break;
+                        } else {
+                            number.push(*next_char);
+                            chars.next();
+                        }
+                    }
+                    result.push(Token {
+                        token_type: TokenType::Error(LexicalError::new("Invalid identifier")),
+                        token_value: number,
+                    });
+                    error = true;
                 }
                 continue;
+            }
+
+            // 字符常数
+            if char == '\'' {
+                let mut constant = char.to_string();
+
+                while let Some(next_char) = chars.peek() {
+                    if *next_char == '\'' {
+                        constant.push(*next_char);
+                        chars.next();
+                        break;
+                    } else {
+                        constant.push(*next_char);
+                        chars.next();
+                    }
+                }
+
+                if constant.len() == 3 {
+                    result.push(Token {
+                        token_type: TokenType::Constant,
+                        token_value: constant,
+                    });
+                } else {
+                    result.push(Token {
+                        token_type: TokenType::Error(LexicalError::new(
+                            "Invalid character constant",
+                        )),
+                        token_value: constant,
+                    });
+                    error = true;
+                }
+                continue;
+            }
+
+            // 字符串常数
+            if char == '"' {
+                let mut constant = char.to_string();
+
+                while let Some(next_char) = chars.peek() {
+                    if *next_char == '"' {
+                        constant.push(*next_char);
+                        chars.next();
+                        break;
+                    } else {
+                        constant.push(*next_char);
+                        chars.next();
+                    }
+                }
+
+                // 字符串首尾必须有双引号
+                if constant.len() >= 2 && constant.starts_with('"') && constant.ends_with('"') {
+                    result.push(Token {
+                        token_type: TokenType::Constant,
+                        token_value: constant,
+                    });
+                } else {
+                    result.push(Token {
+                        token_type: TokenType::Error(LexicalError::new("Invalid string constant")),
+                        token_value: constant,
+                    });
+                    error = true;
+                }
+                continue;
+            }
+
+            // 标识符 & 关键字
+            let mut identifier = char.to_string();
+            while let Some(next_char) = chars.peek() {
+                if next_char == &' '
+                    || OPERATOR.contains(&next_char.to_string().as_str())
+                    || DELIMITERS.contains(&next_char.to_string().as_str())
+                {
+                    break;
+                } else {
+                    identifier.push(*next_char);
+                    chars.next();
+                }
+            }
+
+            if KEYWORDS.contains(&identifier.as_str()) {
+                result.push(Token {
+                    token_type: TokenType::Keyword,
+                    token_value: identifier,
+                });
+            } else {
+                // 首字母应为字母或下划线
+                let first_char = identifier.chars().nth(0).unwrap();
+                if first_char == '_' || first_char.is_alphabetic() {
+                    result.push(Token {
+                        token_type: TokenType::Identifier,
+                        token_value: identifier,
+                    })
+                } else {
+                    result.push(Token {
+                        token_type: TokenType::Error(LexicalError::new("Invalid identifier")),
+                        token_value: identifier,
+                    });
+                }
             }
         }
     }
 
-    Ok(vec![])
+    (result, error)
 }
 
 /// 预处理输入
 ///
 /// 1. 去除注释
 /// 2. 删除首尾空格，删除空行，按空格分割转为 Vec
-fn preprocess(input: String) -> Result<Vec<String>, Box<dyn Error>> {
+fn preprocess(input: String) -> Result<Vec<String>, LexicalError> {
     let input_remove_comment = remove_comment(input)?;
 
     let lines = input_remove_comment
@@ -146,7 +288,7 @@ fn preprocess(input: String) -> Result<Vec<String>, Box<dyn Error>> {
 ///
 /// 单行注释 format: // xxx
 /// 多行注释 format: /* xxx */
-fn remove_comment(input: String) -> Result<String, Box<dyn Error>> {
+fn remove_comment(input: String) -> Result<String, LexicalError> {
     let mut result = String::new();
     let mut chars = input.chars().peekable();
     let mut row = 1;
@@ -192,10 +334,10 @@ fn remove_comment(input: String) -> Result<String, Box<dyn Error>> {
                                 }
                             }
                             if error {
-                                return Err(Box::new(LexicalError::new(&format!(
+                                return Err(LexicalError::new(&format!(
                                     "multiline comment not closed at {}:{}",
                                     start_pos.0, start_pos.1
-                                ))));
+                                )));
                             }
                         }
                         _ => {
@@ -218,6 +360,8 @@ fn remove_comment(input: String) -> Result<String, Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
+    use std::result;
+
     use super::*;
 
     #[test]
@@ -226,15 +370,15 @@ mod tests {
             r#"
 // This is a comment
 int main() {
-    /* 
+/* 
     This is a multiline comment
     int a = 1;
-    */
+*/
     int a = /* this is a inline multiline comment */ 1;
     printf("Hello, world!");
     return 0;
 }
-            "#,
+"#,
         );
         let output = remove_comment(input).unwrap();
         assert_eq!(
@@ -247,7 +391,7 @@ int main() {
     printf("Hello, world!");
     return 0;
 }
-            "#
+"#
         );
     }
 
@@ -265,7 +409,7 @@ int main() {
     printf("Hello, world!");
     return 0;
 }
-            "#,
+"#,
         );
         let result = remove_comment(input);
         assert!(result.is_err());
@@ -273,5 +417,97 @@ int main() {
             result.unwrap_err().to_string(),
             "multiline comment not closed at 5:5"
         );
+    }
+
+    #[test]
+    fn test_preprocess() {
+        let input = String::from(
+            r#"
+// This is a comment
+int main() {
+    /* 
+    This is a multiline comment
+    int a = 1;
+    */
+    int a = /* this is a inline multiline comment */ 1;
+    printf("Hello, world!");
+    return 0;
+    }
+"#,
+        );
+        let output = preprocess(input).unwrap();
+        assert_eq!(
+            output,
+            vec![
+                "int main() {",
+                "int a =  1;",
+                "printf(\"Hello, world!\");",
+                "return 0;",
+                "}"
+            ]
+        );
+    }
+
+    #[test]
+    fn test_lexical_analysis() {
+        let code = String::from(
+            r#"// This is a note.
+            int main(int a, int b){
+                int res;
+                res = a + b;
+                int d = a;
+                /*
+                if (a < 0){
+                    res = -a;
+                }
+                else{
+                    while (b > 0){
+                        b = b-1;
+                    }
+                    res = 0;
+                }
+                */
+            }"#,
+        );
+        let (result, error) = lexical_analysis(code).unwrap();
+        #[rustfmt::skip]
+        assert_eq!(
+            result,
+            vec![
+                Token {token_type: TokenType::Keyword, token_value: "int".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "main".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: "(".to_string()},
+                Token {token_type: TokenType::Keyword, token_value: "int".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "a".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: ",".to_string()},
+                Token {token_type: TokenType::Keyword, token_value: "int".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "b".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: ")".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: "{".to_string()},
+                Token {token_type: TokenType::Keyword, token_value: "int".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "res".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: ";".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "res".to_string()},
+                Token {token_type: TokenType::Operator, token_value: "=".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "a".to_string()},
+                Token {token_type: TokenType::Operator, token_value: "+".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "b".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: ";".to_string()},
+                Token {token_type: TokenType::Keyword, token_value: "int".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "d".to_string()},
+                Token {token_type: TokenType::Operator, token_value: "=".to_string()},
+                Token {token_type: TokenType::Identifier, token_value: "a".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: ";".to_string()},
+                Token {token_type: TokenType::Delimiter, token_value: "}".to_string()}
+            ]
+        );
+        assert!(!error);
+    }
+
+    #[test]
+    fn test() {
+        let identifier = String::from("abc");
+        println!("{}", &identifier[0..1]);
+        assert_eq!(&identifier[0..1], "a");
     }
 }
