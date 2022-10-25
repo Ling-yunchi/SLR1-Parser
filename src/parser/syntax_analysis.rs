482edd2fdfa5b54e314dc666922bb1512dfa07b7
Ling-yunchi <wr020210@gmail.com>
2022-10-25 11:59:01 +0800

    feat: finish get follow

--- src/parser/syntax_analysis.rs
+++ src/parser/syntax_analysis.rs
@@ -3,7 +3,10 @@ use super::{
     lexical_analysis::Token,
 };
 use serde::{Deserialize, Serialize};
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    vec,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Product {
@@ -70,6 +73,9 @@ pub fn syntax_analysis(tokens: Vec<Token>) -> Result<(), SyntaxError> {
         .map_err(|e| SyntaxError::new(&format!("Failed to read grammar file error: {}", e)))?;
     let grammar = Grammar::from_yml(&grammar_yml)
         .map_err(|e| SyntaxError::new(&format!("Failed to parse grammar file error: {}", e)))?;
+    grammar
+        .validate()
+        .map_err(|e| SyntaxError::new(&format!("Grammar valida error: {}", e)))?;
 
     Ok(())
 }
@@ -147,6 +153,7 @@ fn get_first(g: &Grammar) -> HashMap<String, Vec<String>> {
 }
 
 /// 将 y first 集合中的终结符添加到 x first 集合中
+///
 /// @param discard 是否丢弃 y first 集合中的 ε
 /// @return 是否发生了变化
 fn union_first(first: &mut HashMap<String, Vec<String>>, x: &str, y: &str, discard: bool) -> bool {
@@ -179,14 +186,154 @@ fn union_first(first: &mut HashMap<String, Vec<String>>, x: &str, y: &str, disca
     before < after
 }
 
-fn get_follow() -> Result<(), SyntaxError> {
-    Ok(())
+fn get_follow(g: &Grammar) -> HashMap<String, Vec<String>> {
+    let mut first = get_first(g);
+    get_follow_with_first(g, &mut first)
+}
+
+fn get_follow_with_first(
+    g: &Grammar,
+    first: &mut HashMap<String, Vec<String>>,
+) -> HashMap<String, Vec<String>> {
+    let mut follow = HashMap::new();
+
+    // 初始化 follow 集合
+    g.v.iter().for_each(|v| {
+        follow.insert(v.clone(), vec![]);
+    });
+
+    // 将 #(句子结束符) 加入 S 的 follow 集合
+    follow.get_mut(&g.s).unwrap().push("#".to_string());
+
+    // 对于每个产生式 A->αBβ，将 FIRST(β) 去掉ε后加入 FOLLOW(B)
+    let mut changed = true;
+    while changed {
+        changed = false;
+        g.p.iter().for_each(|p| {
+            for i in 0..p.right.len() {
+                // 若产生式右部第一个符号为非终结符，即 X->Y...
+                // 则继续向后遍历直到找到非终结符
+                if !g.v.contains(&p.right[i]) {
+                    continue;
+                }
+
+                // 找到第一个非终结符B
+                // 若产生式为 A -> αB 型，将FOLLOW(A)加入FOLLOW(B)
+                if i == p.right.len() - 1 {
+                    changed = union_follow(&mut follow, &p.right[i], &p.left);
+                    continue;
+                }
+                // 若产生式为 A -> αBβ 型，进行讨论
+                else {
+                    let beta = &p.right[i + 1..].to_vec();
+                    let beta_first = get_first_all(first, beta);
+
+                    // 若β的first集合中含有ε，则同 A -> αB 型，将FOLLOW(A)加入FOLLOW(B)
+                    if beta_first.contains(&"ε".to_string()) {
+                        changed = union_follow(&mut follow, &p.right[i], &p.left);
+                    }
+
+                    // 否则将FITST(β)去除ε后加入FOLLOW(B)
+                    // if !follow.contains_key(&p.right[i]) {
+                    //     follow.insert(p.right[i].clone(), vec![]);
+                    // }
+
+                    let mut b_follow = follow.get(&p.right[i]).unwrap().clone();
+                    let before = b_follow.len();
+
+                    b_follow = b_follow
+                        .into_iter()
+                        .chain(
+                            beta_first
+                                .into_iter()
+                                .filter(|s| s != &&"ε".to_string())
+                                .cloned(),
+                        )
+                        .collect::<HashSet<String>>()
+                        .into_iter()
+                        .collect();
+                    let after = b_follow.len();
+
+                    follow.insert(p.right[i].clone(), b_follow);
+
+                    changed = changed || before < after;
+                }
+            }
+        });
+    }
+
+    follow
+}
+
+/// 将 y follow 集合中的终结符添加到 x follow 集合中
+///
+/// @return x 的 follow 集是否发生了变化
+fn union_follow(follow: &mut HashMap<String, Vec<String>>, x: &str, y: &str) -> bool {
+    let mut x_first = follow.get(x).unwrap().clone();
+    let before = x_first.len();
+
+    let y_first = match y {
+        "ε" => vec!["ε".to_string()],
+        _ => follow.get(y).unwrap().clone(),
+    };
+
+    x_first = x_first
+        .into_iter()
+        .chain(y_first.into_iter())
+        .collect::<HashSet<String>>()
+        .into_iter()
+        .collect();
+    let after = x_first.len();
+
+    follow.insert(x.to_string(), x_first);
+
+    before < after
+}
+
+/// 求a的所有first集，a = Y_1...Y_n
+/// 并将其加入到first集中
+fn get_first_all<'a>(
+    first: &'a mut HashMap<String, Vec<String>>,
+    a: &Vec<String>,
+) -> &'a Vec<String> {
+    // 如果a是单个非终结符或者终结符，则FIRST(a)之前已经求过，直接返回即可
+    if a.len() == 1 {
+        // a = ε 特殊情况
+        // if a.contains(&"ε".to_string()) {
+        //     first.insert("ε".to_string(), vec!["ε".to_string()]);
+        // }
+        return first.get(&a[0]).unwrap();
+    }
+
+    let a_key = a.join(" ");
+    if first.contains_key(&a_key) {
+        return first.get(&a_key).unwrap();
+    }
+
+    first.insert(a_key.clone(), vec![]);
+
+    let mut need_epsilon = true;
+    for i in 0..a.len() {
+        if first.get(&a[i]).unwrap().contains(&"ε".to_string()) {
+            union_first(first, &a_key, &a[i], true);
+        } else {
+            need_epsilon = false;
+            union_first(first, &a_key, &a[i], true);
+            break;
+        }
+    }
+
+    if need_epsilon {
+        union_first(first, &a_key, "ε", false);
+    }
+
+    first.get(&a_key).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::Grammar;
-    use crate::parser::syntax_analysis::get_first;
+    use crate::parser::syntax_analysis::{get_first, get_follow};
 
     #[test]
     fn test_serde_yml_read() {
@@ -209,69 +356,73 @@ mod tests {
         };
     }
 
+    const grammar_yml: &str = r#"
+    s: E
+    v:
+      - E
+      - E'
+      - T
+      - T'
+      - F
+    t:
+      - ε
+      - +
+      - "*"
+      - (
+      - )
+      - id
+    p:
+      - left: E
+        right:
+          - T
+          - E'
+      - left: E'
+        right:
+          - +
+          - T
+          - E'
+      - left: E'
+        right:
+          - ε
+      - left: T
+        right:
+          - F
+          - T'
+      - left: T'
+        right:
+          - "*"
+          - F
+          - T'
+      - left: T'
+        right:
+          - ε
+      - left: F
+        right:
+          - (
+          - E
+          - )
+      - left: F
+        right:
+          - id
+    "#;
+
     #[test]
     fn test_first() {
-        let grammar_yml = r#"
-        s: E
-        v:
-          - E
-          - E'
-          - T
-          - T'
-          - F
-        t:
-          - ε
-          - +
-          - "*"
-          - (
-          - )
-          - id
-        p:
-          - left: E
-            right:
-              - T
-              - E'
-          - left: E'
-            right:
-              - +
-              - T
-              - E'
-          - left: E'
-            right:
-              - ε
-          - left: T
-            right:
-              - F
-              - T'
-          - left: T'
-            right:
-              - "*"
-              - F
-              - T'
-          - left: T'
-            right:
-              - ε
-          - left: F
-            right:
-              - (
-              - E
-              - )
-          - left: F
-            right:
-              - id
-        "#;
         let g = Grammar::from_yml(grammar_yml).unwrap();
         println!("{:#?}", g);
-        let first = get_first(&g);
+        let mut first = get_first(&g);
         println!("{:#?}", first);
+        first.iter_mut().for_each(|(k, v)| {
+            v.sort();
+        });
 
         assert_eq!(
             first,
             hashmap!(
-                s!("E") => vec![s!("id"),s!("(")],
+                s!("E") => vec![s!("("),s!("id")],
                 s!("E'") => vec![s!("+"),s!("ε")],
                 s!("F") => vec![s!("("),s!("id")],
-                s!("T") => vec![s!("id"),s!("(")],
+                s!("T") => vec![s!("("),s!("id")],
                 s!("T'") => vec![s!("*"),s!("ε")],
                 s!("*") => vec![s!("*")],
                 s!("+") => vec![s!("+")],
@@ -279,6 +430,28 @@ mod tests {
                 s!("(") => vec![s!("(")],
                 s!(")") => vec![s!(")")],
                 s!("id") => vec![s!("id")]
+            )
+        );
+    }
+
+    #[test]
+    fn test_follow() {
+        let g = Grammar::from_yml(grammar_yml).unwrap();
+        println!("{:#?}", g);
+        let mut follow = get_follow(&g);
+        println!("{:#?}", follow);
+        follow.iter_mut().for_each(|(k, v)| {
+            v.sort();
+        });
+
+        assert_eq!(
+            follow,
+            hashmap!(
+                s!("E") => vec![s!("#"),s!(")")],
+                s!("E'") => vec![s!("#"),s!(")")],
+                s!("F") => vec![s!("#"),s!(")"),s!("*"),s!("+")],
+                s!("T") => vec![s!("#"),s!(")"),s!("+")],
+                s!("T'") => vec![s!("#"),s!(")"),s!("+")]
             )
         );
     }
